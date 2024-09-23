d440226f43542af3623fca9f142273002c4ae443
Krisha Krel Pascasio <rishsabadi@gmail.com>
2024-09-24 05:33:37 +0800

    finished

---
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.tipcalculator
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.tipcalculator.databinding.ActivityMainBinding
+import com.example.tipcalculator.ui.theme.TipCalculatorTheme
+import java.text.NumberFormat
+import kotlin.math.ceil
+
+class MainActivity : ComponentActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.calculateButton.setOnClickListener { calculateTip() }
+
+    }
+
+    private fun calculateTip() {
+        val costInString = binding.costOfService.text.toString()
+        val cost = costInString.toDoubleOrNull()
+        if (cost == null){
+            binding.tipResult.text = ""
+            return
+        }
+        val tipPercentage = when( binding.tipOptions.checkedRadioButtonId){
+            R.id.option_twenty_percent -> .20
+            R.id.option_eighteen_percent -> .18
+            else -> {.2}
+        }
+        var tip = cost*tipPercentage
+        if (binding.roundTip.isChecked){
+            tip = ceil(tip)
+        }
+        val currencyTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip_amount, currencyTip)
+    }
+}
